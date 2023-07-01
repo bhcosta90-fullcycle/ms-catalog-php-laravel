cff448754c6959110a6021472a0069a41769a732
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-01 13:26:59 -0300

    finalizado

--- app/Services/Storage/FileStorage.php
+++ app/Services/Storage/FileStorage.php
@@ -3,21 +3,29 @@
 namespace App\Services\Storage;
 
 use BRCas\CA\UseCase\FileStorageInterface;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileStorage implements FileStorageInterface
 {
-    /**
-     * @param string $path
-     * @param array $_FILES[file]
-     * @return string
-     */
     public function store(string $path, array $file): string
     {
-        return '';
+        $contents = $this->convertoFileToLaravelFile($file);
+        return Storage::put($path, $contents);
     }
 
     public function delete(string $path): bool
     {
-        return true;
+        return Storage::delete($path);
+    }
+
+    protected function convertoFileToLaravelFile(array $file): UploadedFile
+    {
+        return new UploadedFile(
+            path: $file['tmp_name'],
+            originalName: $file['name'],
+            mimeType: $file['type'],
+            error: $file['error'],
+        );
     }
 }
---
+++ tests/Feature/App/Services/Storage/FileStorage.php
@@ -0,0 +1,39 @@
+<?php
+
+use BRCas\CA\UseCase\FileStorageInterface;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+test("store", function (){
+    $fakeFile = UploadedFile::fake()->create('video.mp', 1, 'video/mp4');
+
+    $file = [
+        'tmp_name' => $fakeFile->getPathname(),
+        'name' => $fakeFile->getClientOriginalName(),
+        'type' => $fakeFile->getType(),
+        'error' => $fakeFile->getError(),
+    ];
+
+    /**
+     * @var FileStorageInterface
+     */
+    $fileStore = app(FileStorageInterface::class);
+    $path = $fileStore->store('videos', $file);
+
+    Storage::assertExists($path);
+    Storage::delete($path);
+});
+
+test("delete", function(){
+    $file = UploadedFile::fake()->create('video.mp', 1, 'video/mp4');
+
+    $path = $file->store('videos');
+
+    /**
+     * @var FileStorageInterface
+     */
+    $fileStore = app(FileStorageInterface::class);
+    expect($fileStore->delete($path))->toBeTrue();
+
+    Storage::assertMissing($path);
+});
